6c4f3dc38835f305e986f6a90aa7084e6ffa39aa
0lio21 <kenyiar90@gmail.com>
2024-10-22 01:12:14 -0300

    Update verificarToken.js

--- Middlewares/verificarToken.js
+++ Middlewares/verificarToken.js
@@ -6,17 +6,19 @@ dotenv.config();
 const secretKey = process.env.SECRET_KEY;
 
 const verificarToken = (req, res, next) => {
-  let token = req.headers['authorization']?.split(' ')[1];  // Tomar token de Authorization
+  // Tomar token desde encabezado Authorization
+  let token = req.headers['authorization']?.split(' ')[1];
   
+  // Si no se encuentra en el header, buscar en el cuerpo de la solicitud
   if (!token) {
-    token = req.query.token || req.body.token || req.cookies.token;  // Buscar token en otras partes
+    token = req.body.token || req.query.token;  // Búsqueda adicional en el cuerpo o en los query params
   }
 
   if (!token) {
     return res.status(403).json({ message: 'Token no proporcionado' });
   }
 
-  jwt.verify(token, secretKey, (err, decoded) => {
+  jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
     if (err) {
       return res.status(403).json({ message: 'Token inválido o expirado' });
     }
